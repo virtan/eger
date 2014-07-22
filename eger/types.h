64bad44e5c0176cd36eec33f1e9ebbe5b655d5f7
Igor Milyakov <imilyakov@machinezone.com>
2014-07-21 20:23:20 -0700

    str to enum conversion

--- eger/types.h
+++ eger/types.h
@@ -35,6 +35,24 @@ struct log_stream : public std::ostringstream {
 
 const size_t log_level_size = ((size_t) level_debug_mare) + 1;
 
+inline log_level str_to_error_level(const string &s) {
+    if(s.empty()) return level_critical;
+    switch(s[0]) {
+        case 'c': return level_critical;
+        case 'e': return level_error;
+        case 'i': return level_info;
+        case 'w': return level_warning;
+        case 'p': return level_profile;
+        case 'd':
+            if(s.size() < 7) return level_debug;
+            switch(s[6]) {
+                case 'h': return level_debug_hard;
+                case 'm': return level_debug_mare;
+            }
+    }
+    return level_critical;
+}
+
 }
 
 #endif
